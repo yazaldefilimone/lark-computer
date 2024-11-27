1403c5e65760c9d28d5aff4ccd91ebe910b3e9b2
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-11-27 04:28:14 +0200

    refactor: move mux to gates

--- src/gates.rs
+++ src/gates.rs
@@ -3,6 +3,10 @@ pub type Bit4 = [Bit; 4];
 pub type Bit8 = [Bit; 8];
 pub type Bit16 = [Bit; 16];
 
+fn eq_bit(a: Bit, b: Bit) -> Bit {
+  not(xor(a, b))
+}
+
 // NAND GATE (BASE)
 // a  | b  | ->
 // ---+----+----
@@ -31,7 +35,7 @@ pub fn not(a: Bit) -> Bit {
 //  1 |  0 |  0
 //  1 |  1 |  1
 pub fn and(a: Bit, b: Bit) -> Bit {
-  nand(nand(a, b), nand(a, b))
+  not(nand(a, b))
 }
 
 // OR GATE
@@ -80,10 +84,10 @@ pub fn not_16(a: Bit16) -> Bit16 {
 
 pub fn and_16(a: Bit16, b: Bit16) -> Bit16 {
   let mut out = [0; 16];
-  for (pos, (bit_a, bit_b)) in a.into_iter().zip(b.into_iter()).enumerate() {
-    out[pos] = and(bit_a, bit_b);
+  for i in 0..16 {
+    out[i] = and(a[i], b[i]);
   }
-  return out;
+  out
 }
 
 // 16-bit OR
@@ -153,11 +157,111 @@ pub fn add_16(a: Bit16, b: Bit16) -> Bit16 {
 
 pub fn inc_16(a: Bit16) -> Bit16 {
   let mut out = [0; 16];
-  let mut carry = 0;
+  let mut carry = 1;
   for pos in (0..16).rev() {
-    let (sum, new_carry) = full_adder(a[pos], 1, carry);
+    let (sum, new_carry) = full_adder(a[pos], 0, carry);
     out[pos] = sum;
     carry = new_carry;
   }
+  out
+}
+
+type Bit2 = [Bit; 2];
+type Bit3 = [Bit; 3];
+// MULTIPLEXER GATE
+//
+// sel | ->
+// ----+---
+//  0  | a
+//  1  | b
+pub fn mux(a: Bit, b: Bit, sel: Bit) -> Bit {
+  return if sel == 0 { a } else { b };
+  // (a & !sel) | (b & sel)
+}
+
+// DEMULTIPLEXER GATE
+//
+// sel | a |  b
+// ----+---+---
+//  0  | n |  0
+//  1  | 0 |  n
+//
+pub fn dmux(n: Bit, sel: Bit) -> (Bit, Bit) {
+  if sel == 0 {
+    return (n, 0);
+  }
+  return (0, n);
+}
+
+// 16-bit MUX
+//
+// a    = 1 | 0 | 1 | 0 | 1 | ....
+// b    = 0 | 1 | 1 | 0 | 0 | ....
+// sel  = 0 | 1 | 0 | 1 | 0 | ....
+//       ---+---+---+---+---+
+// ->   = 0 | 0 | 1 | 0 | 0 | ....
+//
+pub fn mux16(a: Bit16, b: Bit16, sel: Bit) -> Bit16 {
+  let mut out = [0; 16]; // TODO: use a slice
+  for (pos, (bit_a, bit_b)) in a.into_iter().zip(b.into_iter()).enumerate() {
+    out[pos] = mux(bit_a, bit_b, sel);
+  }
   return out;
+}
+
+// 4-way MUX
+//
+// sel0 | sel1 | ->
+// -----+------+---
+//   0  |   0  |  a
+//   1  |   0  |  b
+//   0  |   1  |  c
+//   1  |   1  |  d
+
+pub fn mux4way16(a: Bit16, b: Bit16, c: Bit16, d: Bit16, sel: Bit2) -> Bit16 {
+  match sel {
+    [0, 0] => a,
+    [1, 0] => b,
+    [0, 1] => c,
+    [1, 1] => d,
+    _ => panic!("error: sel value is not valid!"),
+  }
+}
+
+pub fn mux8way16(a: Bit16, b: Bit16, c: Bit16, d: Bit16, e: Bit16, f: Bit16, g: Bit16, h: Bit16, sel: Bit3) -> Bit16 {
+  match sel {
+    [0, 0, 0] => a,
+    [0, 0, 1] => b,
+    [0, 1, 0] => c,
+    [0, 1, 1] => d,
+    [1, 0, 0] => e,
+    [1, 0, 1] => f,
+    [1, 1, 0] => g,
+    [1, 1, 1] => h,
+    _ => panic!("error: sel value is not valid!"),
+  }
+}
+
+pub fn dmux4way(n: Bit, sel: Bit2) -> (Bit, Bit, Bit, Bit) {
+  match sel {
+    [0, 0] => (n, 0, 0, 0),
+    [1, 0] => (0, n, 0, 0),
+    [0, 1] => (0, 0, n, 0),
+    [1, 1] => (0, 0, 0, n),
+    _ => panic!("error: sel value is not valid!"),
+  }
+}
+
+pub fn dmux8way(n: Bit, sel: Bit3) -> (Bit, Bit, Bit, Bit, Bit, Bit, Bit, Bit) {
+  match sel {
+    [0, 0, 0] => (n, 0, 0, 0, 0, 0, 0, 0),
+    [0, 0, 1] => (0, n, 0, 0, 0, 0, 0, 0),
+    [0, 1, 0] => (0, 0, n, 0, 0, 0, 0, 0),
+    [0, 1, 1] => (0, 0, 0, n, 0, 0, 0, 0),
+    [1, 0, 0] => (0, 0, 0, 0, n, 0, 0, 0),
+    [1, 0, 1] => (0, 0, 0, 0, 0, n, 0, 0),
+    [1, 1, 0] => (0, 0, 0, 0, 0, 0, n, 0),
+    [1, 1, 1] => (0, 0, 0, 0, 0, 0, 0, n),
+    _ => panic!("error: sel value is not valid!"),
+  }
 }
